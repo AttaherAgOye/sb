bd9631c99943f640c7aa5d84ddfbafe7e8318efe
Attaher Ag Oye <attaher.oye@etu.univ-nantes.fr>
2026-04-29 09:27:56 +0000

    final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,7 @@
     <meta name="msapplication-TileColor" content="#4F46E5">
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="antialiased">
     @include('layouts.navbar')
